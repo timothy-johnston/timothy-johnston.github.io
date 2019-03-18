d43b904c40e2b0219aacf4b468acd705cf1882a9
Tim Johnston <tedwardj11@gmail.com>
2019-03-18 05:58:05 -0400

    styled navbar: fixed to top & toggled transparency based on scroll

--- javascript/index.js
+++ javascript/index.js
@@ -4,6 +4,19 @@ $( document ).ready(function() {
 
     console.log("hi!");
 
+    //Enable/disable navbar dark background based on scroll location
+    //Code example: https://stackoverflow.com/questions/46503011/how-to-make-navbar-background-appear-when-scrolling-down-but-disappear-when-scr
+    $(window).on("scroll", function() {
+        var scrollPos = $(window).scrollTop();
+        if (scrollPos <= 0) {
+            $('.navbar').addClass('bg-transparent');
+            $('.navbar').removeClass('bg-dark');
+        } else {
+            $('.navbar').removeClass('bg-transparent');
+            $('.navbar').addClass('bg-dark');
+        }
+    });
+
 });
 
 
