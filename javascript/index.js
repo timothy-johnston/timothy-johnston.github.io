4f476917dd54f72e571deafdaa8e9b9a3782193c
Tim Johnston <tedwardj11@gmail.com>
2019-03-26 08:58:01 -0400

    cleaning up javascript modal code

--- javascript/index.js
+++ javascript/index.js
@@ -56,32 +56,25 @@ $( document ).ready(function() {
             }
     }
 
+    //Modal
+    var modal = $('#myModal')[0];
+    var modalOpenBtn = $('#myBtn')[0];
+    var modalCloeBtn= $('.close')[0];
 
-    // Get the modal
-var modal = document.getElementById('myModal');
+    //Open modal 
+    modalOpenBtn.onclick = function() {
+        modal.style.display = "block";
+    }
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+    //Close modal (when x is clicked or outside the window is clicked)
+    modalCloseBtn.onclick = function() {
+        modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 
 
 });
